86702a6a82f061e79dfe4bc14c7411744a50349f
Gordiig <gordiig@gmail.com>
2020-09-28 14:49:16 +0300

    Add 1_Lab (to rule)

--- 1_Lab/Rule.py
+++ 1_Lab/Rule.py
@@ -2,6 +2,10 @@ from typing import List
 from Fact import Fact
 
 
+class RuleError(Exception):
+    pass
+
+
 class Rule:
     AND = '^'
     OR = '|'
@@ -13,6 +17,32 @@ class Rule:
         if rule_type not in (self.AND, self.OR):
             raise ValueError('rule_type must be either Rule.OR, or Rule.AND')
         self.rule_type = rule_type
+
+    @staticmethod
+    def init_from_str(string: str, facts_list: List[Fact]) -> 'Rule':
+        try:
+            lhs, rhs = string.split('>')
+        except ValueError as e:
+            raise RuleError('Wrong rule format') from e
+        lhs = lhs.strip()
+        rhs = rhs.strip()
+        if Rule.AND in lhs:
+            lhs1, lhs2 = lhs.split(Rule.AND)
+            rule_type = Rule.AND
+        elif Rule.OR in lhs:
+            lhs1, lhs2 = lhs.split(Rule.OR)
+            rule_type = Rule.OR
+        else:
+            raise RuleError('Wrong rule format')
+        lhs1 = lhs1.strip()
+        lhs2 = lhs2.strip()
+        try:
+            lhs1_fact = [x for x in facts_list if x.name == lhs1][0]
+            lhs2_fact = [x for x in facts_list if x.name == lhs2][0]
+            rhs_fact = [x for x in facts_list if x.name == rhs][0]
+        except IndexError as e:
+            raise RuleError('Can\'t find needed fact') from e
+        return Rule(in_facts=[lhs1_fact, lhs2_fact], out_fact=rhs_fact, rule_type=rule_type)
 
     def __eq__(self, other):
         if not isinstance(other, Rule):
--- 1_Lab/main.py
+++ 1_Lab/main.py
@@ -1,29 +1,24 @@
-from Fact import Fact
 from Graph import Graph
-from Rule import Rule
+from Reader import Reader
 
 if __name__ == '__main__':
-    print('Initializing facts')
-    facts = [
-        Fact(name=f'{x}') for x in range(1, 12)
-    ]
 
-    print('Initializing rules')
-    rules = [
-        Rule(rule_type=Rule.AND, in_facts=[facts[10], facts[9]], out_fact=facts[2]),
-        Rule(rule_type=Rule.AND, in_facts=[facts[4],  facts[5]], out_fact=facts[3]),
-        Rule(rule_type=Rule.AND, in_facts=[facts[3],  facts[6]], out_fact=facts[1]),
-        Rule(rule_type=Rule.AND, in_facts=[facts[6],  facts[7]], out_fact=facts[1]),
-        Rule(rule_type=Rule.AND, in_facts=[facts[2],  facts[1]], out_fact=facts[0]),
-    ]
+    print('Reading file')
+    reader = Reader('file')
+    facts, rules = reader.get_facts_and_rules()
+    print(f'Facts: {facts}')
+    print('Rules: ')
+    for rule in rules:
+        print(rule)
+    print()
 
     print('Initializing graph')
     graph = Graph(facts=facts, rules=rules)
 
     print('Initializing true facts')
-    true_facts = [facts[2], facts[4], facts[5], facts[6], facts[7]]
+    true_facts = [facts[6], facts[7], facts[9], facts[10]]
 
     print('Starting search')
-    is_reachable = graph.data_to_target(true_facts=true_facts, target_fact=facts[0], with_stack=False)
+    is_reachable = graph.data_to_target(true_facts=true_facts, target_fact=facts[0], with_stack=True)
 
     print(f'Is reachable is {is_reachable}')
