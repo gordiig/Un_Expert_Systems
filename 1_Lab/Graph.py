dd4487c2cd892e93be652061a0b0008465b1810a
Gordiig <gordiig@gmail.com>
2020-10-07 10:43:43 +0300

    2_Lab

--- 1_Lab/Graph.py
+++ 1_Lab/Graph.py
@@ -38,6 +38,10 @@ class Graph:
                     return True
         return True
 
+    def __get_rules_by_out_fact(self, fact: Fact) -> List[Rule]:
+        return [rule for rule in self.rules if rule.out_fact == fact]
+
+    # region Поиск от данных
     def data_to_target(self, true_facts: List[Fact], target_fact: Fact, with_stack: bool = True) -> bool:
         for fact in self.facts:
             fact.watched = False
@@ -66,3 +70,37 @@ class Graph:
             self.__add_to_mem(out_fact)
             self.__scan_and_add_rules_to_container()
         return ans
+    # endregion
+
+    # region Поиск от цели
+    def target_to_data(self, true_facts: List[Fact], target_fact: Fact) -> bool:
+        return self._ttd_fact_job(true_facts=true_facts, target_fact=target_fact)
+
+    def _ttd_fact_job(self, true_facts: List[Fact], target_fact: Fact) -> bool:
+        # Если доказываемый факт в правдивых, то он доказан, возвращаем True
+        if target_fact in true_facts:
+            return True
+        # Получаем правила, которые ведут в факт
+        rules_to_target_fact = self.__get_rules_by_out_fact(fact=target_fact)
+        # Рекурсивно доказываем каждое правило, и если хотя бы одно из них доказано, то доказан и факт
+        for rule in rules_to_target_fact:
+            if self._ttd_rule_job(rule=rule, true_facts=true_facts):
+                print(rule)
+                return True
+        # Если не доказано ни одно из правил, то и факт не доказан
+        return False
+
+    def _ttd_rule_job(self, rule: Rule, true_facts: List[Fact]) -> bool:
+        # Для каждого входящего факта в правило рекурсивно проверяем его доказываемость
+        for fact in rule.in_facts:
+            current_fact_is_true = self._ttd_fact_job(true_facts=true_facts, target_fact=fact)
+            # Если правило типа ИЛИ, и проверяемый факт доказан, то и правило доказано
+            if rule.rule_type == Rule.OR and current_fact_is_true:
+                return True
+            # Если правило типа И, и проверяемый факт недоказуем, то и правило недоказуемо
+            elif rule.rule_type == Rule.AND and not current_fact_is_true:
+                return False
+        # Если правило типа И, и до сих пор не вышли, то все факты доказаны
+        # Если правило типа ИЛИ, и до сих пор не вышли, то ни один из фактов не доказан
+        return rule.rule_type == Rule.AND
+    # endregion
--- 1_Lab/main.py
+++ 1_Lab/main.py
@@ -1,10 +1,10 @@
 from Graph import Graph
 from Reader import Reader
 
-if __name__ == '__main__':
 
-    print('Reading file')
-    reader = Reader('file')
+def data_to_target():
+    print('Reading dtt_file')
+    reader = Reader('dtt_file')
     facts, rules = reader.get_facts_and_rules()
     print(f'Facts: {facts}')
     print('Rules: ')
@@ -22,3 +22,32 @@ if __name__ == '__main__':
     is_reachable = graph.data_to_target(true_facts=true_facts, target_fact=facts[0], with_stack=True)
 
     print(f'Is reachable is {is_reachable}')
+
+
+def target_to_data():
+    print('Reading dtt_file')
+    reader = Reader('ttd_file')
+    facts, rules = reader.get_facts_and_rules()
+    print(f'Facts: {facts}')
+    print('Rules: ')
+    for rule in rules:
+        print(rule)
+    print()
+
+    print('Initializing graph')
+    graph = Graph(facts=facts, rules=rules)
+
+    print('Initializing true facts')
+    true_fact_names = [10, 11, 5, 6, 7]
+    true_facts = [fact for fact in facts if int(fact.name) in true_fact_names]
+    print(f'True facts: {true_facts}')
+
+    print('\nStarting search')
+    is_reachable = graph.target_to_data(true_facts=true_facts, target_fact=facts[0])
+
+    print(f'Is reachable is {is_reachable}')
+
+
+if __name__ == '__main__':
+    # data_to_target()
+    target_to_data()
